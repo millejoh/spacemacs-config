6cea43b439e4bcf5c6307a9010e0478d2e9252ea
John Miller <millejoh@mac.com>
2018-02-15 21:48:07 -0600

    Starting common configuration.
    This should be good for Windows machines, but need to try it out on my one and
    only Linux box.

--- user.el
+++ user.el
@@ -1,3 +1,15 @@
+;;; Configure smartparens
+
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+
+;;; Useful commands for windows
+
 (defun copy-for-ccm (start end)
   (interactive
    (list (region-beginning) (region-end)))
