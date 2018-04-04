8ce10638b66d53f09213ea868cf6dce41177f038
John Miller <millejoh@mac.com>
2018-04-04 13:49:11 -0500

    Evil bindings for ein, better paredit cooperation.

--- user.el
+++ user.el
@@ -1,15 +1,16 @@
 ;;; Configure smartparens
 
-(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
-(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
-(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
-(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
-(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
-(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+(when (require 'paredit nil 'noerror)
+  (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+  (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+  (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+  (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+  (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+  (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+  (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+  (add-hook 'emacs-lisp-mode-hook 'evil-paredit-mode))
 
 ;;; Useful commands for windows
-
 (defun copy-for-ccm (start end)
   (interactive
    (list (region-beginning) (region-end)))
